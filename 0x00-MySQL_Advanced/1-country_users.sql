1738e3b5ddfa62dbee7dda8a33cf8cf1a23d155b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-12 20:27:15 +0300

    create users table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a users table
+
+-- This query reates a table with three columns
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
