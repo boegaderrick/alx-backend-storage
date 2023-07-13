77aa3f147bc4767dd7a36b538186f210c8c8473b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-13 17:14:40 +0300

    create view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,6 @@
 -- This script creates a view
 CREATE VIEW need_meeting AS (
-	SELECT * FROM students WHERE score < 80
+	SELECT name FROM students WHERE score < 80
 	AND (last_meeting IS NULL OR
 	TIMESTAMPDIFF(MONTH, last_meeting, CURDATE()) > 1)
 );
