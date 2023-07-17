841de7fe9760f8081f33a118e4241b8392404bf1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-18 00:15:33 +0300

    update document - pymongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that interacts with a mongodb"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """This function updates a mongodb collection"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
