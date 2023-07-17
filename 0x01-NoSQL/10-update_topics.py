737cd8d25337b298cf7b1a989939dafdc87178f8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-18 00:19:35 +0300

    update document - pymongo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """This function updates a mongodb collection"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
