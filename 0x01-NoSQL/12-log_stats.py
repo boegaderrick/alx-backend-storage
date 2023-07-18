4a9e5a904bb0b35c42b30f97c7438fb22c45154e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-18 17:55:59 +0300

    log parsing - pymongo

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,14 +5,14 @@ from pymongo import MongoClient
 if __name__ == '__main__':
     collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
 
-    print(collection.estimated_document_count(), 'logs')
+    print(collection.count_documents({}), 'logs')
 
     print('Methods:')
-    print('\tmethod GET: ', collection.count_documents({"method": "GET"}))
-    print('\tmethod POST: ', collection.count_documents({"method": "POST"}))
-    print('\tmethod PUT: ', collection.count_documents({"method": "PUT"}))
-    print('\tmethod PATCH: ', collection.count_documents({"method": "PATCH"}))
-    print('\tmethod DELETE: ', collection.count_documents({"method": "DELETE"}))
+    print('\tmethod GET:', collection.count_documents({"method": "GET"}))
+    print('\tmethod POST:', collection.count_documents({"method": "POST"}))
+    print('\tmethod PUT:', collection.count_documents({"method": "PUT"}))
+    print('\tmethod PATCH:', collection.count_documents({"method": "PATCH"}))
+    print('\tmethod DELETE:', collection.count_documents({"method": "DELETE"}))
 
     result = collection.count_documents({"method": "GET", "path": "/status"})
     print(result, 'status check')
