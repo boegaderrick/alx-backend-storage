34d76b89f579623f4211ef13b1958dbf31b5e6d6
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-18 17:25:55 +0300

    log parsing - pymongo

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This script extracts details regarding http requests from a database"""
+from pymongo import MongoClient
+
+collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+
+print(collection.count_documents({}), 'logs')
+
+print('Methods:')
+print('\tmethod GET: ', collection.count_documents({"method": "GET"}))
+print('\tmethod POST: ', collection.count_documents({"method": "POST"}))
+print('\tmethod PUT: ', collection.count_documents({"method": "PUT"}))
+print('\tmethod PATCH: ', collection.count_documents({"method": "PATCH"}))
+print('\tmethod DELETE: ', collection.count_documents({"method": "DELETE"}))
+
+result = collection.count_documents({"method": "GET", "path": "/status"})
+print(result, 'status check')
