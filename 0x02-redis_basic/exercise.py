38bb6b36efaf5f08bcc2ee69bc96232f04cd03ea
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-19 18:30:27 +0300

    add get methods to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module contains Cache class"""
 from redis import Redis
-from typing import Union
+from typing import Any, Callable, Union
 from uuid import uuid4
 
 
@@ -16,6 +16,24 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """This method sets a key-value pair in the database"""
-        key = str(uuid4())
+        key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Union[Callable, None]) -> Any:
+        """Retrieves data and converts it to desired format using 'fn'"""
+        value: Union[bytes, None] = self._redis.get(key)
+        if value and fn:
+            try:
+                value = fn(value)
+            except TypeError:
+                pass
+        return value
+
+    def get_str(self, key: str) -> str:
+        """This method gets string values from the database"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """This method gets int values from the database"""
+        return self.get(key, int)
